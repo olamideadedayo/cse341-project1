3fbbde6f24b962ef31069dbda1b60c2aa762228f
olamideadedayo <olamideadedayo001@gmail.com>
2026-09-16 03:39:23 +0100

    Add dynamic ID route to contacts router

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 router.get('/', async (req, res) => {
   try {
-    const result = await mongodb.getDb().db().collection('contacts').find();
-    const lists = await result.toArray();
+    const result = await mongodb.getDb().db('CSE341').collection('contacts').find();
+    const contacts = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
+    res.status(200).json(contacts);
   } catch (err) {
-    console.error('Error getting contacts:', err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-    const lists = await result.toArray();
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  } catch (err) {
-    console.error('Error getting single contact:', err);
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
